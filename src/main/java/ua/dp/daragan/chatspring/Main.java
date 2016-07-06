2ead650bcc7c0f944f0a82fafdada95c2266f0d6
Bogdan <dar-bodya@i.ua>
2016-07-06 16:54:52 +0300

    Spring

--- src/main/java/ua/dp/daragan/chatspring/Main.java
+++ src/main/java/ua/dp/daragan/chatspring/Main.java
@@ -8,6 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
     public static void main(String[] args) {
+        
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("SpringXML.xml");
 
             while (true){ 
